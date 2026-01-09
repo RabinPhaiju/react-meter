5c0fc57dd270cda313416ba08fbe6249f1532614
Rabin Phaiju <rabinphaiju15@gmail.com>
2026-01-09 09:09:40 +0545

    Fix increment/decrement button handlers to use 0.1 step instead of 1.1

--- react-meter/src/App.jsx
+++ react-meter/src/App.jsx
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="App">
       <Meter value={value} onChange={setValue} />
-      <button onClick={() => setValue(value + 1.1)}>Increment by 0.1</button>
-      <button onClick={() => setValue(Math.max(0, value - 1.1))}>Decrement by 0.1</button>
+      <button onClick={() => setValue(value + 0.1)}>Increment by 0.1</button>
+      <button onClick={() => setValue(Math.max(0, value - 0.1))}>Decrement by 0.1</button>
     </div>
   )
 }
