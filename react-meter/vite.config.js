afbdaef0ee130a964a012f2e12acf41bea6307a4
Rabin Phaiju <rabinphaiju15@gmail.com>
2026-01-09 09:02:12 +0545

    Add gh-pages for deployment and update homepage in package.json; enhance vite.config.js with base path comment

--- react-meter/vite.config.js
+++ react-meter/vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// Replace 'your-username' and 'your-repo' below with your actual GitHub username and repository name
 export default defineConfig({
+  base: '/react-meter/',
   plugins: [react()],
 })
